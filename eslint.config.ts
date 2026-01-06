5c2b43736316e194cbff5c8b0f3d0f5209f1399d
Raine <randomrandom.epic.epic@gmail.com>
2026-01-06 17:56:18 -0500

    feat: git and eslint changes

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,10 +3,31 @@ import isentinel from "@isentinel/eslint-config";
 export default isentinel(
 	{
 		name: "project/core",
+		namedConfigs: true,
 
 		pnpm: true,
 		react: true,
 		type: "package",
 	},
-	{ name: "project/rules", rules: { "ts/explicit-function-return-type": "off" } },
+	{
+		name: "project/rules",
+		rules: {
+			"package-json/require-exports": "off",
+			"ts/explicit-function-return-type": "off",
+		},
+	},
+	{
+		name: "project/test-rules",
+		files: ["test/**"],
+		rules: {
+			"package-json/require-attribution": "off",
+			"package-json/require-author": "off",
+			"package-json/require-description": "off",
+			"package-json/require-exports": "off",
+			"package-json/require-files": "off",
+			"package-json/require-keywords": "off",
+			"package-json/require-license": "off",
+			"package-json/require-types": "off",
+		},
+	},
 );
