697f402a2fc0363db59610719130a90e97d5448c
Raine <randomrandom.epic.epic@gmail.com>
2026-01-06 20:51:53 -0500

    feat: update imports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export function makeHello(str: string) {
 	return `Hello, ${str}!`;
 }
+
+export * from "./components";
+
+// Providers
+export * from "./providers/plugin-provider";
+
+// Util
+export * from "./util/create-plugin";
